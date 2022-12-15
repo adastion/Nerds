a08b2b867f0855d6a6a1898cca6f995fd407a1c4
Andrey Sery <andrey.72006@gmail.com>
2022-12-15 23:45:36 +0300

    Fixed and adding catalog page

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,27 @@
-'use strict'
+'use strict';
 // navigation button
 const btnMenu = document.querySelector('.header__btn-menu');
-const btnClose = document.querySelector('.modal__close'); 
-const menu = document.querySelector('.navigation'); 
+const btnClose = document.querySelector('.modal__close');
+const menu = document.querySelectorAll('.navigation');
 const visually = document.querySelector('.show');
 
-btnMenu.addEventListener('click', () => {
-  menu.classList.toggle('show');
+btnMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  menu.forEach((element) => {
+    element.classList.toggle('show');
+  });
 });
 
 //modal write us
 const modalBtn = document.querySelector('.contacts__btn');
 const modalWriteUs = document.querySelector('.modal');
 
-modalBtn.addEventListener('click', () => {
+modalBtn.addEventListener('click', (e) => {
+  e.preventDefault();
   modalWriteUs.classList.add('show');
 });
 
-btnClose.addEventListener('click', () => {
+btnClose.addEventListener('click', (e) => {
+  e.preventDefault();
   modalWriteUs.classList.remove('show');
 });
